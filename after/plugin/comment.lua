849822a4906feacd6c7a6de546dd1d08b061a779
jan146 <anurjan@gmail.com>
2024-05-07 16:28:32 +0200

    Comment plugin keybindings remap

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -1,1 +1,16 @@
-require('Comment').setup()
+require('Comment').setup({
+    ---LHS of toggle mappings in NORMAL mode
+    toggler = {
+        ---Line-comment toggle keymap
+        line = '<leader>cc',
+        ---Block-comment toggle keymap
+        block = '<leader>cb',
+    },
+    ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+    opleader = {
+        ---Line-comment keymap
+        line = 'cc',
+        ---Block-comment keymap
+        block = 'cb',
+    },
+})

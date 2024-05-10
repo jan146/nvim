4e73db116b5ed99176d7883891d67a7239a4181c
jan146 <anurjan@gmail.com>
2024-05-10 20:55:12 +0200

    Enable preview for colorscheme picker

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,3 +5,11 @@ vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>cs', builtin.colorscheme, {})
+
+require "telescope".setup {
+    pickers = {
+        colorscheme = {
+            enable_preview = true
+        }
+    }
+}

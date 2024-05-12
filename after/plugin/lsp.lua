562f632f4006b329390e9256ae76926b477b3c04
jan146 <anurjan@gmail.com>
2024-05-12 21:59:11 +0200

    Custom keybindings for LSP actions

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,7 +3,22 @@ local lsp_zero = require('lsp-zero')
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+  -- lsp_zero.default_keymaps({buffer = bufnr})
+  local opts = {buffer = bufnr}
+
+  vim.keymap.set('n', '<leader>li', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+  vim.keymap.set('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+  vim.keymap.set('n', '<leader>lD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+  -- vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+  vim.keymap.set('n', '<leader>lt', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+  vim.keymap.set('n', '<leader>lu', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+  vim.keymap.set('n', '<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+  vim.keymap.set('n', '<leader>lr', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+  vim.keymap.set({'n', 'x'}, '<leader>lf', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+  -- vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  -- vim.keymap.set('n', '<leader>gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+  -- vim.keymap.set('n', '<leader>[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+  -- vim.keymap.set('n', '<leader>]d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
 end)
 
 -- to learn how to use mason.nvim

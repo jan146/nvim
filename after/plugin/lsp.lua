fe77146c9e5589a895922e9b1d7af121a9891000
jan146 <anurjan@gmail.com>
2024-12-28 15:40:44 +0100

    Added volar+ts support

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -26,13 +26,44 @@ end)
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {
-      "tailwindcss", "ts_ls", "cssls", "html",
+      "tailwindcss", "ts_ls", "cssls", "html", "volar",
       "lua_ls", "basedpyright", "clangd",
       "bashls",
   },
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
+    end,
+    volar = function()
+      require('lspconfig').volar.setup({})
+    end,
+    ts_ls = function()
+      local vue_typescript_plugin = require('mason-registry')
+        .get_package('vue-language-server')
+        :get_install_path()
+        .. '/node_modules/@vue/language-server'
+        .. '/node_modules/@vue/typescript-plugin'
+
+      require('lspconfig').ts_ls.setup({
+        init_options = {
+          plugins = {
+            {
+              name = "@vue/typescript-plugin",
+              location = vue_typescript_plugin,
+              languages = {'javascript', 'typescript', 'vue'}
+            },
+          }
+        },
+        filetypes = {
+          'javascript',
+          'javascriptreact',
+          'javascript.jsx',
+          'typescript',
+          'typescriptreact',
+          'typescript.tsx',
+          'vue',
+        },
+      })
     end,
   },
 })
